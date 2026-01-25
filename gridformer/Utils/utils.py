666afbc1b4dc8860eb99e030a50045f519644e28
ErnestBeckham <ernestbeckham1998@gmail.com>
2026-01-25 09:36:13 +0530

    helper func for safe tensor

--- gridformer/Utils/utils.py
+++ gridformer/Utils/utils.py
@@ -14,3 +14,19 @@ class Config:
         with open(file_path, 'r') as file:
             data = yaml.safe_load(file)
         return cls(data)
+    
+
+
+
+def _to_jsonable(obj):
+    """
+    Convert common config objects into JSON-serializable structure.
+    """
+    if isinstance(obj, (str, int, float, bool)) or obj is None:
+        return obj
+    if isinstance(obj, (list, tuple)):
+        return [_to_jsonable(x) for x in obj]
+    if isinstance(obj, dict):
+        return {str(k): _to_jsonable(v) for k, v in obj.items()}
+    # fallback (last resort)
+    return str(obj)
